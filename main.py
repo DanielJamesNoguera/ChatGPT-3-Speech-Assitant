6c36c2350cf7773291a1689df85c29d48e266939
Daniel James <103861427+ArclightDaniel@users.noreply.github.com>
2022-12-05 18:10:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -32,9 +32,7 @@ def sendToAi(speech):
         model = Replicate.models.get("stability-ai/stable-diffusion")
         version = model.versions.get("27b93a2413e7f36cd83da926f3656280b2931564ff050bf9575f1fdf9bcd7478")
 
-        pr = speech.split("picture of", 1)
-        print(pr[1])
-        output = version.predict(prompt=pr[1])
+        output = version.predict(prompt=speech)
         print(output[0])
         speak("Sure. Take a look.")
     else:
